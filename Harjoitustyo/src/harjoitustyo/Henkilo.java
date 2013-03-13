773ac812820f3f13c2af7e333471a9bb839812d9
Petteri T Voutilainen <petteriv@dcb-2.cs.helsinki.fi>
2013-03-13 12:15:10 +0200

    paivitys

--- Harjoitustyo/src/harjoitustyo/Henkilo.java
+++ Harjoitustyo/src/harjoitustyo/Henkilo.java
@@ -10,4 +10,50 @@ package harjoitustyo;
  */
 public class Henkilo {
     
+    private String nimi;
+    private int HP;
+    private int panssari;
+    private int voima;
+    private int lvl;
+    private boolean elava;
+    
+    public Henkilo(String nimi){
+        this.nimi = nimi;
+        this.HP = 100;
+        this.panssari = 10;
+        this.voima = 20;
+        this.lvl = 1;
+        this.elava = true;
+    }
+    
+    public String getNimi(){
+        return this.nimi;
+    }
+        
+    public int aiheutaVahinkoa(){
+        return this.voima;
+    }
+    
+    public int otaVahinkoa(int vahinko){
+        this.HP = this.HP - vahinko;
+        return this.HP;
+    }
+    
+    public boolean kuolema(){
+        if(this.HP<=0){
+            this.elava = false;
+        }
+        return this.elava;
+    }
+    
+    public int lisaaHP(int lisaHP){
+        this.HP = this.HP + lisaHP;
+        return this.HP;
+    }
+    
+    public int lisaaVoimaa(int lisaVoima){
+        this.voima = this.voima + lisaVoima;
+        return this.voima;
+    }
+    
 }
